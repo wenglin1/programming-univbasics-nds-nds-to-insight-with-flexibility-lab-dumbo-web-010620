a95691ec334ba960ac93fac83687c1ff1cbbe4fd
weng lin <wenglin@buffalo.edu>
2020-01-03 06:26:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -70,7 +70,7 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-  result {}
+  result = {}
   i = 0
   while i < collection.count
     studio_name = collection[i][:studio]
